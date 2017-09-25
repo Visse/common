0b6b5c0db964cf29fea9b99c343b77ea4fcf87e5
visse <visse44@gmail.com>
2017-09-25 18:16:11 +0200

    Added overload of BuiltinFromString that doesn't take string length

--- BuiltinFromString.h
+++ BuiltinFromString.h
@@ -6,4 +6,10 @@ namespace Common
 {
     template< typename Type >
     COMMON_API bool BuiltinFromString( const char *str, size_t len, Type &value );
+
+    template< typename Type >
+    bool BuiltinFromString( const char *str, Type &value )
+    {
+        return BuiltinFromString(str, strlen(str), value);
+    }
 }
