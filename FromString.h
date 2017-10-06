7788a826ee130e6adeea1bf919deea206f628ae4
visse <visse44@gmail.com>
2017-10-07 01:20:36 +0200

    Added overload for bool to FromString & added variant that doesn't take the string length

--- FromString.h
+++ FromString.h
@@ -7,10 +7,14 @@ namespace Common
 #define _BUILTIN_FROM_STRING( Type )                                        \
     inline bool FromString( const char *str, size_t len, Type &value ) {    \
         return BuiltinFromString(str, len, value);                          \
+    }                                                                       \
+    inline bool FromString( const char *str, Type &value ) {                \
+        return BuiltinFromString(str, value);                               \
     }
     _BUILTIN_FROM_STRING(int32_t);
     _BUILTIN_FROM_STRING(uint32_t);
     _BUILTIN_FROM_STRING(float);
     _BUILTIN_FROM_STRING(double);
+    _BUILTIN_FROM_STRING(bool);
 #undef _BUILTIN_FROM_STRING
 }
