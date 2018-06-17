0e9e23b5bb41d4b0f8dc1c36c99fc5d7af8b330b
visse <visse44@gmail.com>
2018-06-17 15:14:49 +0200

    Made DECLARE_ENUM_FLAG generated  functions static

--- EnumFlagOp.h
+++ EnumFlagOp.h
@@ -3,42 +3,42 @@
 #include <type_traits>
 
 #define DECLARE_ENUM_FLAG( enum )                                   \
-    inline constexpr enum operator | ( enum e1, enum e2 ) {         \
+    inline static constexpr enum operator | ( enum e1, enum e2 ) {  \
         return static_cast<enum>(                                   \
             std::underlying_type<enum>::type(e1) |                  \
             std::underlying_type<enum>::type(e2)                    \
         );                                                          \
     }                                                               \
-    inline constexpr enum operator & ( enum e1, enum e2 ) {         \
+    inline static constexpr enum operator & ( enum e1, enum e2 ) {  \
         return static_cast<enum>(                                   \
             std::underlying_type<enum>::type(e1) &                  \
             std::underlying_type<enum>::type(e2)                    \
         );                                                          \
     }                                                               \
-    inline constexpr enum operator ^ ( enum e1, enum e2 ) {         \
+    inline static constexpr enum operator ^ ( enum e1, enum e2 ) {  \
         return static_cast<enum>(                                   \
             std::underlying_type<enum>::type(e1) ^                  \
             std::underlying_type<enum>::type(e2)                    \
         );                                                          \
     }                                                               \
-    inline constexpr enum operator ~ ( enum e1 ) {                  \
+    inline static constexpr enum operator ~ ( enum e1 ) {           \
         return static_cast<enum>(                                   \
             ~std::underlying_type<enum>::type(e1)                   \
         );                                                          \
     }                                                               \
-    inline constexpr bool any( enum e, enum flags ) {               \
+    inline static constexpr bool any( enum e, enum flags ) {        \
         return std::underlying_type<enum>::type(e&flags) != 0;      \
     }                                                               \
-    inline constexpr bool all( enum e, enum flags ) {               \
+    inline static constexpr bool all( enum e, enum flags ) {        \
         return (e&flags) == flags;                                  \
     }                                                               \
-    inline constexpr bool none( enum e, enum flags ) {              \
+    inline static constexpr bool none( enum e, enum flags ) {       \
         return std::underlying_type<enum>::type(e&flags) == 0;      \
     }                                                               \
-    inline constexpr enum clear( enum e, enum flags ) {             \
+    inline static constexpr enum clear( enum e, enum flags ) {      \
         return e & (~flags);                                        \
     }                                                               \
-    inline constexpr enum set( enum e, enum flags ) {               \
+    inline static constexpr enum set( enum e, enum flags ) {        \
         return e | flags;                                           \
     }                                                               \
     
